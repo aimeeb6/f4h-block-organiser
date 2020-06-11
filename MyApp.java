3357c109357ea36ac716cb5279823d20b93d325f
Aimee Boyle <aimeeboyle6@gmail.com>
2020-06-11 16:38:58 +0100

    Update of file directories

--- MyApp.java
+++ MyApp.java
@@ -6,8 +6,8 @@ import java.util.Comparator;
 import java.io.FileWriter;
 import java.io.IOException;
 public class MyApp {
-    private static String livePathname = "/Users/Aimee/Downloads/EHR_Work/Development/forms4health-form-definitions";
-    private String f4hPathname = "/Users/Aimee/Downloads/EHR_Work/Development/ltht-form-definitions";
+    private static String livePathname = "/Users/aimeeboyle/Documents/EHR Work/Development/forms4health-form-definitions";
+    private String f4hPathname = "/Users/aimeeboyle/Documents/EHR Work/Development/airelogic/ltht-form-definitions";
     public FormDirectory liveDir = new FormDirectory(livePathname);
     public FormDirectory f4hDir = new FormDirectory(f4hPathname);
     private ArrayList<Block> listOfBlockObjects = new ArrayList<>();
